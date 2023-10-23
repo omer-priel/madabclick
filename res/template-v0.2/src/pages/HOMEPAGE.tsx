73d75a4c0f0c3fd4608cd8f45f5d89e37b8070eb
Omer Priel <omer.priel2@gmail.com>
2023-10-23 15:24:27 +0300

    include template-v0.2

---
+++ res/template-v0.2/src/pages/HOMEPAGE.tsx
@@ -0,0 +1,283 @@
+import { FunctionComponent } from "react";
+
+const HOMEPAGE: FunctionComponent = () => {
+  return (
+    <div className="relative bg-gray-100 w-full h-[4188px] overflow-hidden text-right text-base text-black font-running-text">
+      <img
+        className="absolute top-[1119px] left-[0px] w-[1920px] h-[1055px] object-cover"
+        alt=""
+        src="/image-8@2x.png"
+      />
+      <div className="absolute top-[1118px] left-[0px] [background:linear-gradient(186.82deg,_#b1dfef,_rgba(217,_217,_217,_0))] w-[1930px] h-[426px]" />
+      <div className="absolute top-[1118px] left-[0px] bg-white w-[1920px] h-[2518px] hidden" />
+      <img
+        className="absolute top-[158px] left-[0px] w-[1920px] h-[960px] object-cover"
+        alt=""
+        src="/rectangle-18@2x.png"
+      />
+      <div className="absolute top-[158px] left-[0px] [background:linear-gradient(-75.99deg,_rgba(0,_0,_0,_0.5)_27.99%,_rgba(0,_0,_0,_0)_96.51%)] w-[1920px] h-[960px]" />
+      <div className="absolute top-[430px] left-[1227px] text-xl uppercase font-black text-white inline-block w-[492px] h-[172px]">
+        <p className="m-0">&nbsp;</p>
+        <p className="m-0">
+          אתם מוזמנים לצפות איתי כל היום וכל הלילה בסרטונים מדעיים, מרתקים
+          ומסקרנים בכל התחומים.
+        </p>
+        <p className="m-0">{`היזהרו ואל תשכחו מה הסקרנות עשתה לחתול . . . `}</p>
+        <p className="m-0">&nbsp;</p>
+      </div>
+      <div className="absolute top-[556px] left-[1227px] uppercase text-white inline-block w-[492px] h-7">
+        הכניסה למעבדה מותרת מעל גיל 5 ולילדים עם יכולת ריכוז מעל 5 דקות.
+      </div>
+      <div className="absolute top-[388px] left-[1100px] text-21xl uppercase font-black text-white">
+        ברוכים הבאים למעבדה של אסכולה
+      </div>
+      <div className="absolute top-[2252px] left-[calc(50%_-_133px)] text-21xl uppercase font-black">
+        כניסה למעבדה
+      </div>
+      <div className="absolute top-[1459px] left-[calc(50%_-_189px)] text-21xl uppercase font-black">
+        המלצת בוגרי אסכולה
+      </div>
+      <div className="absolute top-[43px] left-[1669px] w-[129px] h-6 text-yellowgreen">
+        <div className="absolute top-[0px] left-[83px] w-[46px] h-6">
+          <b className="absolute top-[0px] left-[0px] [text-decoration:underline] uppercase">
+            עברית
+          </b>
+        </div>
+        <div className="absolute top-[0px] left-[0px] w-[47px] h-6 text-black">
+          <div className="absolute top-[0px] left-[0px] uppercase">אנגלית</div>
+          <div className="absolute top-[0px] left-[0px] uppercase">אנגלית</div>
+        </div>
+      </div>
+      <div className="absolute top-[2348px] left-[634px] w-[652px] h-[25px] text-left">
+        <div className="absolute top-[0px] left-[545px] w-[107px] h-[25px]">
+          <b className="absolute top-[0px] left-[0px] uppercase">מתי נולדת?</b>
+          <img
+            className="absolute top-[0px] left-[82px] w-[25px] h-[25px]"
+            alt=""
+            src="/dashiconsarrowup.svg"
+          />
+        </div>
+        <div className="absolute top-[0px] left-[266px] w-[186px] h-[25px]">
+          <b className="absolute top-[0px] left-[0px] uppercase">
+            מהי יכולת הריכוז שלך?
+          </b>
+          <img
+            className="absolute top-[0px] left-[161px] w-[25px] h-[25px]"
+            alt=""
+            src="/dashiconsarrowup.svg"
+          />
+        </div>
+        <div className="absolute top-[0px] left-[0px] w-[173px] h-[25px]">
+          <b className="absolute top-[0px] left-[0px] uppercase">
+            מהי שפת האם שלך?
+          </b>
+          <img
+            className="absolute top-[0px] left-[148px] w-[25px] h-[25px]"
+            alt=""
+            src="/dashiconsarrowup.svg"
+          />
+        </div>
+      </div>
+      <div className="absolute top-[1194px] left-[calc(50%_-_322px)] w-[644px] h-16 text-gray-400">
+        <div className="absolute top-[0px] left-[calc(50%_-_322px)] rounded-31xl bg-white shadow-[1px_1px_1px_rgba(0,_0,_0,_0.25)] box-border w-[644px] h-16 border-[0.5px] border-solid border-black" />
+        <div className="absolute top-[5px] left-[calc(50%_+_23px)] w-[267px] h-[55px]">
+          <div className="absolute top-[4px] left-[calc(50%_-_318.5px)] rounded-31xl bg-gray-300 box-border w-[476px] h-12 hidden border-[1px] border-solid border-gray-400" />
+          <div className="absolute top-[15px] left-[0px] uppercase font-light inline-block w-[188px] h-[26px]">
+            מה מסקרן אותך היום?
+          </div>
+          <img
+            className="absolute top-[0px] left-[calc(50%_+_78.5px)] w-[55px] h-[55px] overflow-hidden"
+            alt=""
+            src="/vaadinglasses.svg"
+          />
+        </div>
+      </div>
+      <img
+        className="absolute top-[30px] left-[calc(50%_-_141px)] w-[281.14px] h-[97.13px]"
+        alt=""
+        src="/group-7.svg"
+      />
+      <img
+        className="absolute top-[38px] left-[1833px] w-[35px] h-[35px] overflow-hidden"
+        alt=""
+        src="/pajamasearth.svg"
+      />
+      <div className="absolute top-[2321.5px] left-[calc(50%_-_65.5px)] box-border w-[131px] h-[5px] border-t-[5px] border-solid border-yellowgreen" />
+      <div className="absolute top-[1530.5px] left-[calc(50%_-_65.5px)] box-border w-[131px] h-[5px] border-t-[5px] border-solid border-yellowgreen" />
+      <div className="absolute top-[2513px] left-[549px] bg-gainsboro w-[400px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[2513px] left-[982px] bg-gainsboro w-[400px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[2513px] left-[1415px] bg-gainsboro w-[400px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[2513px] left-[1848px] bg-gainsboro w-[400px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[2444px] left-[calc(50%_-_106px)] text-5xl uppercase font-black">{`פיזיקה ואסטרונומיה `}</div>
+      <div className="absolute top-[2513px] left-[116px] bg-gainsboro w-[400px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <img
+        className="absolute top-[2513px] left-[1448px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[2513px] left-[1013px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[2513px] left-[578px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[2513px] left-[143px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[2513px] left-[0px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-6@2x.png"
+      />
+      <div className="absolute top-[2810px] left-[1557px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[2810px] left-[1122px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[2810px] left-[687px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[2810px] left-[252px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <img
+        className="absolute top-[2600px] left-[0px] w-[104px] h-[108px]"
+        alt=""
+        src="/iconamoonarrowup2.svg"
+      />
+      <div className="absolute top-[2999px] left-[435.08px] bg-gainsboro w-[435.08px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[2999px] left-[906.05px] bg-gainsboro w-[435.08px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[2932px] left-[calc(50%_-_63px)] text-5xl uppercase font-black text-center inline-block w-[127px]">
+        מתמטיקה
+      </div>
+      <div className="absolute top-[2999px] left-[116px] bg-gainsboro w-[400px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <img
+        className="absolute top-[2999px] left-[1448px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-11@2x.png"
+      />
+      <img
+        className="absolute top-[2999px] left-[1024px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[2999px] left-[578px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[2999px] left-[143px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[2999px] left-[0px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-6@2x.png"
+      />
+      <div className="absolute top-[3296px] left-[1557px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[3296px] left-[1122px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[3296px] left-[687px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[3296px] left-[252px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <img
+        className="absolute top-[3086px] left-[0px] w-[104px] h-[108px]"
+        alt=""
+        src="/iconamoonarrowup2.svg"
+      />
+      <div className="absolute top-[3485px] left-[435.08px] bg-gainsboro w-[435.08px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[3485px] left-[906.05px] bg-gainsboro w-[435.08px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[3485px] left-[1377.03px] bg-gainsboro w-[435.08px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[3485px] left-[1848px] bg-gainsboro w-[435.08px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <div className="absolute top-[3418px] left-[calc(50%_-_49px)] text-5xl uppercase font-black text-center inline-block w-[97.89px]">
+        כימיה
+      </div>
+      <div className="absolute top-[3485px] left-[116px] bg-gainsboro w-[400px] h-[274px] [transform:_rotate(180deg)] [transform-origin:0_0]" />
+      <img
+        className="absolute top-[3485px] left-[1448px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[3485px] left-[1013px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[3485px] left-[578px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[3485px] left-[143px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-1@2x.png"
+      />
+      <img
+        className="absolute top-[3485px] left-[0px] w-[400px] h-[282px] object-cover"
+        alt=""
+        src="/image-6@2x.png"
+      />
+      <div className="absolute top-[3782px] left-[1557px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[3782px] left-[1122px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[3782px] left-[687px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <div className="absolute top-[3782px] left-[252px] uppercase font-black">
+        מבוא לפיזיקה - חוקי ניוטון
+      </div>
+      <img
+        className="absolute top-[3572px] left-[0px] w-[104px] h-[108px]"
+        alt=""
+        src="/iconamoonarrowup21.svg"
+      />
+      <div className="absolute top-[1308px] left-[512px] w-[897px] h-[50px] text-xl text-white">
+        <div className="absolute top-[0px] left-[705px] bg-gray-200 w-48 h-[50px]" />
+        <b className="absolute top-[10px] left-[756px] uppercase text-yellowgreen">
+          חוקי ניוטון
+        </b>
+        <div className="absolute top-[0px] left-[470px] bg-gray-200 w-48 h-[50px]" />
+        <div className="absolute top-[10px] left-[502px] uppercase font-light">
+          מסע בין כוכבים
+        </div>
+        <div className="absolute top-[0px] left-[235px] bg-gray-200 w-48 h-[50px]" />
+        <div className="absolute top-[10px] left-[289px] uppercase font-light">
+          חוקי ניוטון
+        </div>
+        <div className="absolute top-[0px] left-[0px] bg-gray-200 w-48 h-[50px]" />
+        <div className="absolute top-[10px] left-[32px] uppercase font-light">
+          מסע בין כוכבים
+        </div>
+      </div>
+      <img
+        className="absolute top-[1560px] left-[calc(50%_-_360px)] w-[720px] h-[480px] object-cover"
+        alt=""
+        src="/image-2@2x.png"
+      />
+      <div className="absolute top-[2057px] left-[calc(50%_-_147px)] uppercase font-black">
+        מומלץ! החפרנים: ספיישל מסע בין כוכבים
+      </div>
+    </div>
+  );
+};
+
+export default HOMEPAGE;
