d27c51dac9c026c831ffcf5bad5644eb45c3b057
Omer Priel <omer.priel2@gmail.com>
2023-10-25 20:20:20 +0300

    terraform apply

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,6 +1,6 @@
 resource "aws_network_interface" "frontend" {
   subnet_id       = aws_subnet.prod_1.id
-  private_ips     = ["10.0.1.51", "10.0.1.52", "10.0.1.53", "10.0.1.54"]
+  private_ips     = ["10.0.1.50", "10.0.1.51", "10.0.1.52", "10.0.1.53", "10.0.1.54", "10.0.1.55", "10.0.1.56"]
   security_groups = [aws_security_group.frontend.id]
 
   tags = {
@@ -18,8 +18,32 @@ resource "aws_launch_template" "frontend" {
   }
 }
 
-resource "aws_autoscaling_group" "frontend" {
-  name = "frontend"
+resource "aws_autoscaling_group" "frontend_blue" {
+  name = "frontend_blue"
+
+  vpc_zone_identifier = [aws_subnet.prod_1.id, aws_subnet.prod_2.id]
+  target_group_arns   = [aws_lb_target_group.frontend.arn]
+
+  force_delete = true
+
+  desired_capacity = 2
+  min_size         = 2
+  max_size         = 2
+
+  tag {
+    key                 = "Name"
+    value               = "frontend"
+    propagate_at_launch = true
+  }
+
+  launch_template {
+    id      = aws_launch_template.frontend.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_autoscaling_group" "frontend_green" {
+  name = "frontend_green"
 
   vpc_zone_identifier = [aws_subnet.prod_1.id, aws_subnet.prod_2.id]
   target_group_arns   = [aws_lb_target_group.frontend.arn]
--- deployment.tf
+++ deployment.tf
@@ -43,7 +43,7 @@ resource "aws_codedeploy_deployment_group" "frontend" {
   app_name              = aws_codedeploy_app.frontend.name
   deployment_group_name = "frontend"
   service_role_arn      = aws_iam_role.prod_codedeploy.arn
-  autoscaling_groups    = [aws_autoscaling_group.frontend.id]
+  autoscaling_groups    = [aws_autoscaling_group.frontend_blue.id, aws_autoscaling_group.frontend_green.id]
 
   tags = {
     Name = "frontend"
