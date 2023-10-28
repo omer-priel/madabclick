6be346fb23bef6834a273b3c59ca50745cea65a1
Omer Priel <omer.priel2@gmail.com>
2023-10-28 23:59:29 +0300

    add Appliction Load Balancer

--- infra/load_balancer.tf
+++ infra/load_balancer.tf
@@ -10,28 +10,29 @@ resource "aws_lb" "frontend" {
   }
 }
 
-# resource "aws_lb_target_group" "frontend" {
-#   name     = "frontend"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.prod.id
+resource "aws_lb_target_group" "frontend" {
+  name        = "frontend"
+  target_type = "instance"
+  vpc_id      = aws_vpc.prod.id
+  protocol    = "HTTP"
+  port        = 80
 
-#   tags = {
-#     Name = "frontend"
-#   }
-# }
+  tags = {
+    Name = "frontend"
+  }
+}
 
-# resource "aws_lb_listener" "frontend" {
-#   load_balancer_arn = aws_lb.frontend.arn
-#   port              = "80"
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.frontend.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.frontend.arn
-#   }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
 
-#   tags = {
-#     Name = "frontend"
-#   }
-# }
+  tags = {
+    Name = "frontend"
+  }
+}
