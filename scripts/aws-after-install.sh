8d620632a0800f5e617dd863245155dea6822acf
Omer Priel <omer.priel2@gmail.com>
2023-10-29 07:27:18 +0200

    add logs to /tmp/example.txt

--- scripts/aws-after-install.sh
+++ scripts/aws-after-install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 export NVM_DIR="/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >> /tmp/example.txt
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >> /tmp/example.txt
 
-nvm --version
+nvm --version >> /tmp/example.txt
 
-# nvm use 20.9.0
+nvm use 20.9.0 >> /tmp/example.txt
 
-# node -v
-# yarn -v
+node -v >> /tmp/example.txt
+yarn -v >> /tmp/example.txt
 
-# # build frontend server
-# cd /var/frontend/
+# build frontend server
+cd /var/frontend/ >> /tmp/example.txt
 
-# rm -rf node_modules/ yarn.lock
-# rm -rf .next
+rm -rf node_modules/ yarn.lock >> /tmp/example.txt
+rm -rf .next >> /tmp/example.txt
 
-# yarn install
+yarn install >> /tmp/example.txt
 
-# yarn run build
+yarn run build >> /tmp/example.txt
--- scripts/aws-application-start.sh
+++ scripts/aws-application-start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 export NVM_DIR="/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >> /tmp/example.txt
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >> /tmp/example.txt
 
-nvm --version
+nvm --version >> /tmp/example.txt
 
-# nvm use 20.9.0
+nvm use 20.9.0 >> /tmp/example.txt
 
-# node -v
-# yarn -v
+node -v >> /tmp/example.txt
+yarn -v >> /tmp/example.txt
 
-# # Start the server
-# cd /var/frontend/
+# Start the server
+cd /var/frontend/ >> /tmp/example.txt
 
-# yarn run start
+yarn run start >> /tmp/example.txt
