02fe245dd43987c09d60fc4e9a9d6ab4183bc1c3
Omer Priel <omer.priel2@gmail.com>
2023-10-29 18:07:01 +0200

    update deploy

---
+++ scripts/aws-after-install-root.sh
@@ -0,0 +1,2 @@
+chmod -R 777 /var/frontend/
+chown -R ec2-user:ec2-user /var/frontend/
--- scripts/aws-application-start.sh
+++ scripts/aws-application-start.sh
@@ -1,16 +1,16 @@
 
-# export NVM_DIR="/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >> /tmp/example.txt
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >> /tmp/example.txt
+export NVM_DIR="/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >> /tmp/example.txt
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" >> /tmp/example.txt
 
-# nvm --version >> /tmp/example.txt
+nvm --version
 
-# nvm use 20.9.0 >> /tmp/example.txt
+nvm use 20.9.0
 
-# node -v >> /tmp/example.txt
-# yarn -v >> /tmp/example.txt
+node -v
+yarn -v
 
-# # Start the server
-# cd /var/frontend/ >> /tmp/example.txt
+# Start the server
+cd /var/frontend
 
-# yarn run start >> /tmp/example.txt
+yarn run start
