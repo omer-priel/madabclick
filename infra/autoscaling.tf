28253ff148fb738411ef44b5ab08426de278c177
Omer Priel <omer.priel2@gmail.com>
2023-10-29 19:36:11 +0200

    last time

--- infra/autoscaling.tf
+++ infra/autoscaling.tf
@@ -38,7 +38,7 @@ resource "aws_autoscaling_group" "frontend" {
   min_size         = 2
   max_size         = 2
 
-  health_check_grace_period = 300
+  health_check_grace_period = 3000
   health_check_type         = "ELB"
 
   vpc_zone_identifier = [aws_subnet.prod_a.id, aws_subnet.prod_b.id]
