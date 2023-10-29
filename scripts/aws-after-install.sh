d7d44c9cec41b37da26cf2baa07b1f3709530598
Omer Priel <omer.priel2@gmail.com>
2023-10-29 02:51:56 +0200

    update aws-after-install.sh script

--- scripts/aws-after-install.sh
+++ scripts/aws-after-install.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 # Install yarn, node and yarn
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+if [ -d "$HOME/.nvm" ]; then
+  echo "NVM is already installed."
+else
+  # Install NVM
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+fi
+
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-nvm install 18.17.0
+# Check if Node.js 18.17.0 is already installed
+if [ "$(nvm ls 18.17.0)" == "v18.17.0" ]; then
+  echo "Node.js v18.17.0 is already installed."
+else
+  nvm install 18.17.0
+fi
+
 npm install --global yarn
 
 # build frontend server
