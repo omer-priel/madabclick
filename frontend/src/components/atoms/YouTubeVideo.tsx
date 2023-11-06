c1e1bfffa310782c6f75639c00dab86a48bf3636
Omer Priel <omer.priel2@gmail.com>
2023-11-06 10:57:50 +0200

    fix-lint

--- frontend/src/components/atoms/YouTubeVideo.tsx
+++ frontend/src/components/atoms/YouTubeVideo.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import YouTube from 'react-youtube';
 
 import { Content } from '@/lib/api/schemas';
-import { getActive, setActive, clearActive } from '@/playersManager';
+import { clearActive, getActive, setActive } from '@/playersManager';
 
 interface Props {
   content: Content;
@@ -21,12 +21,12 @@ export default function YouTubeVideo({ content, width, height }: Props) {
     return <></>;
   }
 
-  // autoplay: 1,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const playerVars: any = {
     width,
     height,
     enablejsapi: 1,
-    origin: null
+    origin: null,
   };
 
   if (content.youtube.playlist) {
@@ -51,50 +51,50 @@ export default function YouTubeVideo({ content, width, height }: Props) {
         </button>
       ) : (
         <YouTube
-        className='w-full h-full rounded-[10px] bg-transparent'
-        iframeClassName='w-full h-full rounded-[10px]'
-        title={content.title}
-        videoId={content.youtube.id}
-        opts={{ playerVars }}
-        onReady={(e) => {
-          console.log("onReady", e);
-          e.target.playVideo();
-        }}
-        onPlay={(e) => {
-          console.log("onPlay", e);
-          const last = getActive();
-          if (last.activePlayer && last.activeContentIndex != content.index) {
-            last.activePlayer.stopVideo();
-          }
-
-          setActive(e.target, content.index);
-        }}
-        onPause={(e) => {
-          console.log("onPause", e);
-          clearActive();
-        }}
-        onEnd={(e) => {
-          console.log("onEnd", e);
-          setShow(false);
-        }}
-        onStateChange={(e) => {
-          console.log("onStateChange", e);
-
-          // start new video
-          if (e.data === 1) {
-            if (e.target.getVideoData().video_id == content.youtube?.id) {
-              return;
-            }
-            if (e.target.getPlaylistId() == content.youtube?.playlist?.id) {
-              return;
+          className='w-full h-full rounded-[10px] bg-transparent'
+          iframeClassName='w-full h-full rounded-[10px]'
+          title={content.title}
+          videoId={content.youtube.id}
+          opts={{ playerVars }}
+          onReady={(e) => {
+            console.log('onReady', e);
+            e.target.playVideo();
+          }}
+          onPlay={(e) => {
+            console.log('onPlay', e);
+            const last = getActive();
+            if (last.activePlayer && last.activeContentIndex != content.index) {
+              last.activePlayer.stopVideo();
             }
 
-            e.target.stopVideo();
+            setActive(e.target, content.index);
+          }}
+          onPause={(e) => {
+            console.log('onPause', e);
             clearActive();
+          }}
+          onEnd={(e) => {
+            console.log('onEnd', e);
             setShow(false);
-          }
-        }}
-      />
+          }}
+          onStateChange={(e) => {
+            console.log('onStateChange', e);
+
+            // start new video
+            if (e.data === 1) {
+              if (e.target.getVideoData().video_id == content.youtube?.id) {
+                return;
+              }
+              if (e.target.getPlaylistId() == content.youtube?.playlist?.id) {
+                return;
+              }
+
+              e.target.stopVideo();
+              clearActive();
+              setShow(false);
+            }
+          }}
+        />
       )}
     </div>
   );
