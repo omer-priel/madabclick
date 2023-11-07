27c91e87f8cc53a6a2b429da7d1b1fc63ab2e69b
Omer Priel <omer.priel2@gmail.com>
2023-11-08 00:10:18 +0200

    fix-lint

--- frontend/src/lib/db/requests.ts
+++ frontend/src/lib/db/requests.ts
@@ -1,15 +1,4 @@
-import { getConfig } from "@/config"
-
-// client connection
-function getDBClient(): {
-
-}
-
 // requests
-export async function getYoutubeDataAPIVideos() {
+export function getYoutubeDataAPIVideos() {}
 
-}
-
-export async function getYoutubeDataAPIPlaylists() {
-
-}
+export function getYoutubeDataAPIPlaylists() {}
