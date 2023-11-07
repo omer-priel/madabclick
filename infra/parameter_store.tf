41778862553fbe5a8cc37ca4ee6f8d00215c53c5
Omer Priel <omer.priel2@gmail.com>
2023-11-08 00:35:08 +0200

    replace data aws_ssm_parameter to resource aws_ssm_parameter

--- infra/parameter_store.tf
+++ infra/parameter_store.tf
@@ -1,11 +1,50 @@
-data "aws_ssm_parameter" "frontend_env_google_api_key" {
-  name = "/frontend/env/google_api_key"
+resource "aws_ssm_parameter" "frontend_env_google_api_key" {
+  name        = "/frontend/env/google_api_key"
+  type        = "String"
+  value       = "-"
+  description = "GOOGLE_API_KEY environment variable for frontend"
+
+  tags = {
+    Name = "frontend"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
 }
 
-data "aws_ssm_parameter" "frontend_env_google_spreadsheet_id_contents" {
-  name = "/frontend/env/google_spreadsheet_id_contents"
+resource "aws_ssm_parameter" "frontend_env_google_spreadsheet_id_contents" {
+  name        = "/frontend/env/google_spreadsheet_id_contents"
+  type        = "String"
+  value       = "-"
+  description = "GOOGLE_SPREADSHEET_ID_CONTENTS environment variable for frontend"
+
+  tags = {
+    Name = "frontend"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
 }
 
-data "aws_ssm_parameter" "frontend_env_mongo_uri" {
-  name = "/frontend/env/mongo_uri"
+resource "aws_ssm_parameter" "frontend_env_mongo_uri" {
+  name        = "/frontend/env/mongo_uri"
+  type        = "String"
+  value       = "-"
+  description = "MONGO_URI environment variable for frontend"
+
+  tags = {
+    Name = "frontend"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
 }
