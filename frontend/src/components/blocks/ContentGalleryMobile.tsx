9cdff2f79ed2785bf1fe8521608478daeb97bca9
Omer Priel <omer.priel2@gmail.com>
2023-12-13 15:29:23 +0200

    remove custom arrows

--- frontend/src/components/blocks/ContentGalleryMobile.tsx
+++ frontend/src/components/blocks/ContentGalleryMobile.tsx
@@ -1,53 +1,14 @@
 'use client';
 
-import { CSSProperties, MouseEventHandler } from 'react';
-
-import Image from 'next/image';
 import Slider from 'react-slick';
 
 import ContentCardMobile from '@/components/blocks/ContentCardMobile';
 
 import { Content } from '@/lib/api/schemas';
-import { cn } from '@/lib/styling';
-import contentsSidebarLeftArrowIcon from '@/public/contents-sidebar-left-arrow.svg';
 import { useStore } from '@/store';
 
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
-
-interface ArrowProps {
-  className?: string;
-  style?: CSSProperties;
-  onClick?: MouseEventHandler;
-}
-
-function PrevArrow({ className, style, onClick }: ArrowProps) {
-  return (
-    <Image
-      className={cn('w-[30px] h-[30px]', className)}
-      style={{ ...style, width: '30px', height: '30px', translate: '0 -100%', rotate: '180deg' }}
-      alt=''
-      src={contentsSidebarLeftArrowIcon}
-      width='30'
-      height='30'
-      onClick={onClick}
-    />
-  );
-}
-
-function NextArrow({ className, style, onClick }: ArrowProps) {
-  return (
-    <Image
-      className={cn('w-[30px] h-[30px]', className)}
-      style={{ ...style, width: '30px', height: '30px' }}
-      alt=''
-      src={contentsSidebarLeftArrowIcon}
-      width='30'
-      height='50'
-      onClick={onClick}
-    />
-  );
-}
 
 interface Props {
   contents: Content[];
@@ -66,9 +27,7 @@ export default function ContentGalleryMobile({ contents }: Props) {
         centerPadding='20px'
         centerMode
         variableWidth
-        arrows={true}
-        prevArrow={<PrevArrow />}
-        nextArrow={<NextArrow />}
+        arrows
         rtl
         afterChange={() => {
           deactivatePlayer();
